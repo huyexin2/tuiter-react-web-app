5629c4c82943de6a66fbe6f74a40afb077fd8df8
huyexin1 <yexin.hu@mail.utoronto.ca>
2023-07-17 23:38:21 -0400

    assignment 2

--- src/App.js
+++ src/App.js
@@ -5,12 +5,32 @@ import Tuiter from "./tuiter/index"
 
 function App() {
     return (
-        <div className="container">
-            <HelloWorld/>
-            <Assignment3/>
-            <Tuiter/>
-        </div>
+        <div className="App">
+            <header className="App-header">
 
+                <h1> Welcome to Web Dev</h1>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
     );
 }
+//     return (
+//         <div className="container">
+//             <HelloWorld/>
+//             <Assignment3/>
+//             <Tuiter/>
+//         </div>
+//
+//     );
+// }
 export default App;
