16a4c3ea6fb8f4114753405320f33f051696ffe0
huyexin1 <yexin.hu@mail.utoronto.ca>
2023-07-17 23:25:53 -0400

    assignment 2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Assignment3 from "./labs/a3";
+import HelloWorld from "./labs/a3/hello-world";
+import Tuiter from "./tuiter/index"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1> Welcome to Web Dev</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+        <div className="container">
+            <HelloWorld/>
+            <Assignment3/>
+            <Tuiter/>
+        </div>
 
+    );
+}
 export default App;
